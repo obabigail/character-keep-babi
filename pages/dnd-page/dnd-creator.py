e6e2d600805a4c82c1937722faa8f0343159b3db
Riquelmy Fernandes <riquelmy.fernandes@agnet.com.br>
2026-04-01 14:45:22 -0300

    started building export functionality

--- pages/dnd-page/dnd-creator.py
+++ pages/dnd-page/dnd-creator.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from PIL import Image
+from spire.pdf import *
+
+sheet = "../character-keep-babi/data/5E_CharacterSheet_Fillable.pdf"
 
 st.set_page_config(
         page_title="DnD Keep",
@@ -8,7 +11,7 @@ st.set_page_config(
     
 st.title("DnD Keep")
 
-dnd_name, dnd_class, dnd_background, dnd_race, dnd_equipment, dnd_stats, dnd_traits, dnd_misc = st.tabs([
+dnd_name, dnd_class, dnd_background, dnd_race, dnd_equipment, dnd_stats, dnd_traits, dnd_misc, dnd_export = st.tabs([
     "Name",
     "Class",
     "Background",
@@ -16,7 +19,8 @@ dnd_name, dnd_class, dnd_background, dnd_race, dnd_equipment, dnd_stats, dnd_tra
     "Equipment",
     "Stats",
     "Traits",
-    "Miscelaneous"]
+    "Miscelaneous",
+    "Export"]
 )
 
 with dnd_name:
@@ -41,3 +45,34 @@ with dnd_name:
     st.markdown(f"# I am the mighty :rainbow[{character_name}]!")
     if character_profile is not None:
         st.image(character_profile, width=500)
+
+with dnd_export:
+    if character_name is not (None or ""):
+        st.header("Behold...")
+        st.subheader("You're ready to export your sheet!")
+        st.caption("Adventure awaits. HUZZAH!")
+
+        dnd_sheet = PdfDocument()
+        dnd_sheet.LoadFromFile(sheet)
+
+        first_page = dnd_sheet.Pages[0]
+
+        font = PdfTrueTypeFont("Arial", 12.0, 0 , False)
+        brush = PdfBrushes.get_Black()
+
+        first_page.Canvas.DrawString(character_name, font, brush, 60.0, 65.0)
+        dnd_sheet.SaveToFile(f"../character-keep-babi/data/{character_name}_D&D5E_Sheet.pdf")
+        dnd_sheet.Close()
+
+        with open(f"../character-keep-babi/data/{character_name}_D&D5E_Sheet.pdf", "rb") as pdf_file:
+            PDFbyte = pdf_file.read()
+
+        st.download_button(
+            "Download character sheet",
+            data=PDFbyte,
+            file_name=f"{character_name}_D&D5E_Sheet.pdf",
+            mime="application/pdf"
+            )
+    else:
+        st.write("You need to finish you character's creation before exporting it!")
+        st.caption("'Hey, I'm not even alive yet!'")
